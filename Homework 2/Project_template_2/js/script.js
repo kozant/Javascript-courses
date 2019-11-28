59870c8ee3d207a89339206d10f86aedc27afcf5
Anton <diamonddiver99@gmail.com>
2019-11-29 00:58:06 +0300

    edit hm2

--- Homework 2/Project_template_2/js/script.js
+++ Homework 2/Project_template_2/js/script.js
@@ -1,8 +1,5 @@
 
-//я ещё не доделал, пока только в таком варианте
-
-
-
+//я ещё не доделал, пока только в таком варианте\
 
 var submit = document.getElementById("submit");
 var ul = document.querySelector("ul");
@@ -10,29 +7,31 @@ var clearBtn = document.querySelector("#clear");
 var saveBtn = document.getElementById("save");
 
 
-var limit = 360; // в секундах
-function processTimer() {
-  if (limit > 0) {
-      setTimeout("processTimer()", 1000);
-      limit--;
-  } else {
-      // здесь действия после завершения таймера
-      //..
-  }
-  var limit_div = parseInt(limit / 60); // минуты
-  var limit_mod = limit - limit_div * 60; // секунды
-  // строка с оставшимся временем
-  limit_str = "&nbsp;&nbsp;";
-  if (limit_div < 10) limit_str = limit_str + "0";
-  limit_str = limit_str + limit_div + ":";
-  if (limit_mod < 10) limit_str = limit_str + "0";
-  limit_str = limit_str + limit_mod + "&nbsp;&nbsp;";
-  // вывод времени
-  var el_timer = document.getElementById("timer");
-  if (el_timer) el_timer.innerHTML = limit_str;
-}
-processTimer();
+function timer(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
 
+    console.log(day);
+    var nowDate = new Date();
+    var achiveDate = new Date(year, month, day, 0, 0, 0); //Задаем дату, к которой будет осуществляться обратный отсчет
+    var result = (achiveDate - nowDate)+1000;
+    if (result < 0) {
+        var elmnt = document.getElementById('timer');
+        elmnt.innerHTML = ' - : - - : - - : - - ';
+        return undefined;
+    }
+    var seconds = Math.floor((result/1000)%60);
+    var minutes = Math.floor((result/1000/60)%60);
+    var hours = Math.floor((result/1000/60/60)%24);
+    var days = Math.floor(result/1000/60/60/24);
+    if (seconds < 10) seconds = '0' + seconds;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (hours < 10) hours = '0' + hours;
+    var elmnt = document.getElementById('timer');
+    elmnt.innerHTML = days + ':' + hours + ':' + minutes + ':' + seconds;
+    setTimeout(timer, 1000);
+}
 
 
 
@@ -43,31 +42,40 @@ function loadTodo(){
   if(localStorage.getItem('todoList')){
     ul.innerHTML = localStorage.getItem("todoList");
     var spanElement = document.querySelectorAll("span");
-    var li = document.querySelectorAll("li");
     for (var i = 0; i < spanElement.length; i++) {
-      var spanTime = document.createElement("span");
-      spanTime.setAttribute("id", "timer");
       deleteTask(spanElement[i]);
-      ul.appendChild(li[i]).append(spanTime);
+      localStorage.removeItem('todoList');
+      var divTime = document.createElement('div');
+      divTime.setAttribute('id', 'timer');
     }
   }
 }
+
 
 //Обработчик каждого события при вводе, чтобы добавлять новое задание в список
 submit.addEventListener("click",function(){
   //Создание нового задания при нажатии на enter 
   var li = document.createElement("li");
   var spanElement = document.createElement("span");
+
   var icon = document.createElement("svg");  
   icon.setAttribute("color", "red");
 
   var newTodo = document.getElementById("task").value;
-  var newDate = document.getElementById("date").value;  
+  var newDate = document.getElementById("date").value;
+
+  var newDay = Number(newDate[0] + newDate[1]);
+  var newMonth = Number(newDate[3] + newDate[4]);
+  var newYear = Number(newDate[6] + newDate[7] + newDate[8] + newDate[9]);
+
+  var divTime = document.createElement('div');
+  divTime.setAttribute('id', 'timer');
 
   icon.classList.add('fas', 'fa-trash-alt'); 
 
   spanElement.append(icon);
-  ul.appendChild(li).append(spanElement, newTodo + " "); //таймер
+  ul.appendChild(li).append(spanElement, newTodo + " ", divTime);
+  timer(newYear, newMonth, newDay); 
 
   deleteTask(spanElement);
 
@@ -83,16 +91,10 @@ function deleteTask(elem){
   })
 }
 
-/*saveBtn.addEventListener('click', function(){
-  if(localStorage.getItem('todoList')){
-    ul.innerHTML = localStorage.getItem("todoList");
-  }
-})*/
-
 //Удаление todolist чтобы 
 clearBtn.addEventListener('click', function(){
   ul.innerHTML= "";
-  localStorage.removeItem("todolist", ul.innerHTML)
+  localStorage.clear();
 });
 
 loadTodo();  
